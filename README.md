3f67aabcdbdd7e7094940a023fc6ac4d0091255e
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-02-28 20:19:53 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,3 @@
+# Extensions for Play 2
+
+Various extensions including MongoDB support
---
+++ project/Build.scala
@@ -0,0 +1,48 @@
+import sbt._
+import Keys._
+
+object Build extends Build {
+
+  val buildVersion = "1.0-SNAPSHOT"
+
+  val delvingReleases = "Delving Releases Repository" at "http://development.delving.org:8081/nexus/content/repositories/releases"
+  val delvingSnapshots = "Delving Snapshot Repository" at "http://development.delving.org:8081/nexus/content/repositories/snapshots"
+  val delvingRepository = if(buildVersion.endsWith("SNAPSHOT")) delvingSnapshots else delvingReleases
+
+  val dependencies = Seq(
+    "play"                 %%    "play"                        % "2.0-RC3-SNAPSHOT",
+    "eu.delving"           %%    "groovy-templates-plugin"     % "1.0-SNAPSHOT",
+    "com.mongodb.casbah"   %%    "casbah"                      % "2.1.5-1",
+    "com.novus"            %%    "salat-core"                  % "0.0.8-SNAPSHOT",
+    "org.joda"             %     "joda-convert"                % "1.2",
+    "commons-collections"  %     "commons-collections"         % "3.2.1",
+    "commons-httpclient"   %     "commons-httpclient"          % "3.1",
+    "commons-io"           %     "commons-io"                  % "2.1",
+    "org.apache.commons"   %     "commons-email"               % "1.2",
+    "commons-lang"         %     "commons-lang"                % "2.6"  )
+
+  val main = Project(
+    id = "play2-extensions",
+    base = file(".")).settings(
+      organization := "eu.delving",
+
+      version := buildVersion,
+
+      resolvers += "scala-tools" at "http://scala-tools.org/repo-releases/",
+
+      resolvers += "novus" at "http://repo.novus.com/snapshots/",
+
+      resolvers += delvingReleases,
+
+      resolvers += delvingSnapshots,
+
+      libraryDependencies ++= dependencies,
+
+      publishTo := Some(delvingRepository),
+
+      credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
+
+      publishMavenStyle := true
+    )
+
+}
