7522f40e3cc25c851517da5b67b0f8c875e0edbd
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2012-06-25 11:44:04 +0200

    1.1

--- project/Build.scala
+++ project/Build.scala
@@ -3,17 +3,17 @@ import Keys._
 
 object Build extends Build {
 
-  val buildVersion = "1.1-SNAPSHOT"
+  val buildVersion = "1.1"
 
   val delvingReleases = "Delving Releases Repository" at "http://development.delving.org:8081/nexus/content/repositories/releases"
   val delvingSnapshots = "Delving Snapshot Repository" at "http://development.delving.org:8081/nexus/content/repositories/snapshots"
   val delvingRepository = if(buildVersion.endsWith("SNAPSHOT")) delvingSnapshots else delvingReleases
 
   val dependencies = Seq(
-    "play"                 %%    "play"                        % "2.0",
-    "eu.delving"           %%    "groovy-templates-plugin"     % "1.3-SNAPSHOT",
+    "play"                 %%    "play"                        % "2.0.1",
+    "eu.delving"           %%    "groovy-templates-plugin"     % "1.3",
     "com.mongodb.casbah"   %%    "casbah"                      % "2.1.5-1",
-    "com.novus"            %%    "salat-core"                  % "0.0.8-SNAPSHOT",
+    "com.novus"            %%    "salat-core"                  % "0.0.8",
     "org.joda"             %     "joda-convert"                % "1.2",
     "commons-collections"  %     "commons-collections"         % "3.2.1",
     "commons-httpclient"   %     "commons-httpclient"          % "3.1",
